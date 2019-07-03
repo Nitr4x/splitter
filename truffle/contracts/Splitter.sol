1bdf1166488e79d71be3c9621f8c9696d0fafb9f
nitr4x <jean.lejeune@protonmail.com>
2019-07-03 19:01:42 +0200

    Delete functionnality

--- truffle/contracts/Splitter.sol
+++ truffle/contracts/Splitter.sol
@@ -3,34 +3,53 @@ pragma solidity 0.5.10;
 import './Owned.sol';
 
 contract Splitter is Owned {
+    
     struct Person {
         address wallet;
         bytes32 name;
-        uint pendingWithdrawals;
-        bool isParticipant;
+        uint    pendingWithdrawals;
+        uint    index;
+        bool    isParticipant;
     }
     
     mapping(address => Person) participantStorage;
     address[] participantList;
+    uint public balance;
 
+    modifier _onlyParticipant {
+        require(participantStorage[msg.sender].isParticipant);
+        _;
+    }
+    
     constructor(bytes32 name) public payable {
+        balance = 0;
         addParticipant(msg.sender, name);
     }
     
-    function isParticipant(address participant) internal view returns(bool) {
-        return participantStorage[participant].isParticipant;    
-    }
-    
     function addParticipant(address participant, bytes32 name) public _onlyOwner returns(bool) {
-        require(!isParticipant(participant));
+        require(!participantStorage[msg.sender].isParticipant);
         
         participantList.push(participant);
         participantStorage[participant].wallet = participant;
         participantStorage[participant].name = name;
         participantStorage[participant].pendingWithdrawals = 0;
+        participantStorage[participant].index = participantList.length;
         participantStorage[participant].isParticipant = true;
         
         return true;
+    }
+    
+    function selfDelete(address participant) public _onlyParticipant returns(bool) {
+        require(msg.sender == participant);
+        
+        uint index = participantStorage[msg.sender].index;
+        
+        participantList[index] = participantList[participantList.length - 1];
+        participantStorage[participantList[index]].index = index;
+        delete(participantList[participantList.length - 1]);
+        participantList.length--;
+        
+        delete(participantStorage[msg.sender]);
     }
     
     function getParticipants() public view returns(address[] memory, bytes32[] memory) {
@@ -45,11 +64,11 @@ contract Splitter is Owned {
         return (addrs, names);
     }
     
-    function sendWei() public payable returns(bool) {
-        require(isParticipant(msg.sender));
+    function sendWei() public _onlyParticipant payable returns(bool) {
         require(msg.value <= msg.sender.balance);
         require(participantList.length > 0);
 
+        balance += msg.value;
         for (uint i = 0; i < participantList.length; i++) {
             if (participantList[i] != msg.sender) {
                 participantStorage[participantList[i]].pendingWithdrawals += msg.value / (participantList.length - 1);
@@ -59,12 +78,11 @@ contract Splitter is Owned {
         return true;
     }
     
-    function withdraw() public returns(bool) {
-        require(isParticipant(msg.sender));
-        
+    function withdraw() public _onlyParticipant returns(bool) {
         uint amount = participantStorage[msg.sender].pendingWithdrawals;
         
         participantStorage[msg.sender].pendingWithdrawals = 0;
+        balance -= amount;
         msg.sender.transfer(amount);
         
         return true;
