03a0b41794388929265bfe0d284e8d8c205bf59d
nitrax <jean.lejeune@protonmail.com>
2019-07-03 20:04:37 +0200

    Updating contracts

--- truffle/contracts/Owned.sol
+++ truffle/contracts/Owned.sol
@@ -1,7 +1,10 @@
 pragma solidity 0.5.10;
 
 contract Owned {
+    
     address public owner;
+    
+    event LogOwnerChanged(address newOwner);
     
     modifier _onlyOwner {
         require(msg.sender == owner);
@@ -10,5 +13,13 @@ contract Owned {
     
     constructor() public {
         owner = msg.sender;
+    }
+    
+    function changeOwner(address newOwner) internal _onlyOwner returns(bool) {
+        owner = newOwner;
+        
+        emit LogOwnerChanged(newOwner);
+        
+        return true;
     }
 }
--- truffle/contracts/Splitter.sol
+++ truffle/contracts/Splitter.sol
@@ -16,6 +16,11 @@ contract Splitter is Owned {
     address[] participantList;
     uint public balance;
 
+    event LogParticipantAdded(address participant, bytes32 name);
+    event LogPariticipantDeleted(address participant);
+    event LogEthSent(address sender, uint amount);
+    event LogWithdrawed(address receiver);
+    
     modifier _onlyParticipant {
         require(participantStorage[msg.sender].isParticipant);
         _;
@@ -27,35 +32,38 @@ contract Splitter is Owned {
     }
     
     function addParticipant(address participant, bytes32 name) public _onlyOwner returns(bool) {
-        require(!participantStorage[msg.sender].isParticipant);
+        require(!participantStorage[participant].isParticipant);
         
-        participantList.push(participant);
         participantStorage[participant].wallet = participant;
         participantStorage[participant].name = name;
         participantStorage[participant].pendingWithdrawals = 0;
         participantStorage[participant].index = participantList.length;
         participantStorage[participant].isParticipant = true;
+        participantList.push(participant);
+
+        emit LogParticipantAdded(participant, name);
         
         return true;
     }
     
-    function selfDelete(address participant) public _onlyParticipant returns(bool) {
-        require(msg.sender == participant);
+    function deleteParticipant(address participant) public _onlyParticipant returns(bool) {
+        require(msg.sender == participant && msg.sender != owner);
         
         uint index = participantStorage[msg.sender].index;
         
         participantList[index] = participantList[participantList.length - 1];
-        participantStorage[participantList[index]].index = index;
-        delete(participantList[participantList.length - 1]);
+        participantStorage[participantList[participantList.length - 1]].index = index;
         participantList.length--;
-        
+
         delete(participantStorage[msg.sender]);
+        
+        emit LogPariticipantDeleted(msg.sender);
     }
     
     function getParticipants() public view returns(address[] memory, bytes32[] memory) {
         address[] memory addrs = new address[](participantList.length);
         bytes32[] memory names = new bytes32[](participantList.length);
-        
+
         for (uint i = 0; i < participantList.length; i++) {
             addrs[i] = participantStorage[participantList[i]].wallet;
             names[i] = participantStorage[participantList[i]].name;
@@ -64,9 +72,9 @@ contract Splitter is Owned {
         return (addrs, names);
     }
     
-    function sendWei() public _onlyParticipant payable returns(bool) {
+    function sendEth() public _onlyParticipant payable returns(bool) {
         require(msg.value <= msg.sender.balance);
-        require(participantList.length > 0);
+        require(participantList.length > 1);
 
         balance += msg.value;
         for (uint i = 0; i < participantList.length; i++) {
@@ -75,7 +83,13 @@ contract Splitter is Owned {
             }
         }
         
+        emit LogEthSent(msg.sender, msg.value);
+        
         return true;
+    }
+    
+    function changeOwnernership(address newOwner) public _onlyParticipant _onlyOwner returns(bool) {
+        return super.changeOwner(newOwner);    
     }
     
     function withdraw() public _onlyParticipant returns(bool) {
@@ -84,6 +98,8 @@ contract Splitter is Owned {
         participantStorage[msg.sender].pendingWithdrawals = 0;
         balance -= amount;
         msg.sender.transfer(amount);
+        
+        emit LogWithdrawed(msg.sender);
         
         return true;
     }
