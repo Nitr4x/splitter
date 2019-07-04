568067a6d669a15dfdaae04e605b4e7b66cc402c
nitrax <jean.lejeune@protonmail.com>
2019-07-04 19:46:53 +0200

    Reducing gas cost

--- truffle/contracts/Splitter.sol
+++ truffle/contracts/Splitter.sol
@@ -4,11 +4,7 @@ import './Stoppable.sol';
 
 contract Splitter is Stoppable {
     
-    struct Account {
-        uint    pendingWithdrawals;
-    }
-    
-    mapping(address => Account) accountStorage;
+    mapping(address => uint) accountStorage;
 
     event LogEthSent(address sender, uint value, address receiver1, address receiver2);
     event LogWithdrawed(address sender, uint amount);
@@ -16,20 +12,20 @@ contract Splitter is Stoppable {
     constructor() public payable {}
     
     function splitEth(address receiver1, address receiver2) public _onlyIfRunning payable returns(bool) {
-        require(msg.value <= msg.sender.balance, 'The sender balance shoud be higher the ether\'s value');
-
-        accountStorage[receiver1].pendingWithdrawals += msg.value / 2;
-        accountStorage[receiver2].pendingWithdrawals += msg.value / 2;
+        uint half = msg.value / 2;
+        
+        accountStorage[receiver1] += half;
+        accountStorage[receiver2] += half;
 
         emit LogEthSent(msg.sender, msg.value, receiver1, receiver2);
         
         return true;
     }
-    
+
     function withdraw() public returns(bool) {
-        uint amount = accountStorage[msg.sender].pendingWithdrawals;
+        uint amount = accountStorage[msg.sender];
         
-        accountStorage[msg.sender].pendingWithdrawals = 0;
+        accountStorage[msg.sender] = 0;
         
         emit LogWithdrawed(msg.sender, amount);
 
