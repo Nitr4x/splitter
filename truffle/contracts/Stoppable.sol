4c8176c481b6016412d072e2c6549324619131e9
nitr4x <jean.lejeune@protonmail.com>
2019-07-04 16:29:46 +0200

    Adding stoppable pattern

---
+++ truffle/contracts/Stoppable.sol
@@ -0,0 +1,38 @@
+pragma solidity 0.5.10;
+
+import './Owned.sol';
+
+contract Stoppable is Owned {
+    
+    bool isRunning;
+    
+    event LogPausedContract(address sender);
+    event LogResumeContract(address sender);
+    
+    modifier _onlyIfRunning {
+        require(isRunning);
+        _;
+    }
+    
+    constructor() public {
+        isRunning = true;
+    }
+    
+    function pauseContract() public _onlyOwner _onlyIfRunning returns(bool) {
+        isRunning = false;
+        
+        emit LogPausedContract(msg.sender);
+        
+        return true;
+    }
+    
+    function resumeContract() public _onlyOwner returns(bool) {
+        require(!isRunning);
+        
+        isRunning = true;
+        
+        emit LogResumeContract(msg.sender);
+        
+        return true;
+    }
+}
