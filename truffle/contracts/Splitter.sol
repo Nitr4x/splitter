43ff1e7cb0269b9d9b918f57e8b5ea19975b0549
nitr4x <jean.lejeune@protonmail.com>
2019-07-04 17:37:16 +0200

    Changing the contract inner working to fit with the project guidance....

--- truffle/contracts/Splitter.sol
+++ truffle/contracts/Splitter.sol
@@ -4,80 +4,23 @@ import './Stoppable.sol';
 
 contract Splitter is Stoppable {
     
-    struct Person {
-        bytes32 name;
+    struct Account {
         uint    pendingWithdrawals;
-        uint    index;
-        bool    isParticipant;
     }
     
-    mapping(address => Person) participantStorage;
-    address[] participantList;
+    mapping(address => Account) accountStorage;
 
-    event LogParticipantAdded(address participant, bytes32 name);
-    event LogPariticipantDeleted(address participant);
     event LogEthSent(address sender, uint amount);
     event LogWithdrawed(address receiver);
     
-    modifier _onlyParticipant {
-        require(participantStorage[msg.sender].isParticipant);
-        _;
-    }
+    constructor() public payable {}
     
-    constructor(bytes32 name) public payable {
-        addParticipant(msg.sender, name);
-    }
-    
-    function addParticipant(address participant, bytes32 name) public _onlyOwner returns(bool) {
-        require(!participantStorage[participant].isParticipant);
-        
-        participantStorage[participant].name = name;
-        participantStorage[participant].pendingWithdrawals = 0;
-        participantStorage[participant].index = participantList.length;
-        participantStorage[participant].isParticipant = true;
-        participantList.push(participant);
-
-        emit LogParticipantAdded(participant, name);
-        
-        return true;
-    }
-    
-    function deleteParticipant(address participant) public _onlyParticipant returns(bool) {
-        require(msg.sender == participant && msg.sender != getOwner());
-        
-        uint index = participantStorage[msg.sender].index;
-        
-        participantList[index] = participantList[participantList.length - 1];
-        participantStorage[participantList[participantList.length - 1]].index = index;
-        participantList.length--;
-
-        delete(participantStorage[msg.sender]);
-        
-        emit LogPariticipantDeleted(msg.sender);
-    }
-    
-    function getParticipants() public view returns(address[] memory, bytes32[] memory) {
-        address[] memory addrs = new address[](participantList.length);
-        bytes32[] memory names = new bytes32[](participantList.length);
-
-        for (uint i = 0; i < participantList.length; i++) {
-            addrs[i] = participantList[i];
-            names[i] = participantStorage[participantList[i]].name;
-        }
-        
-        return (addrs, names);
-    }
-    
-    function sendEth() public _onlyParticipant payable returns(bool) {
+    function splitEth(address receiver1, address receiver2) public _onlyIfRunning payable returns(bool) {
         require(msg.value <= msg.sender.balance);
-        require(participantList.length > 1);
 
-        for (uint i = 0; i < participantList.length; i++) {
-            if (participantList[i] != msg.sender) {
-                participantStorage[participantList[i]].pendingWithdrawals += msg.value / (participantList.length - 1);
-            }
-        }
-        
+        accountStorage[receiver1].pendingWithdrawals += msg.value / 2;
+        accountStorage[receiver2].pendingWithdrawals += msg.value / 2;
+
         emit LogEthSent(msg.sender, msg.value);
         
         return true;
@@ -87,10 +30,10 @@ contract Splitter is Stoppable {
         return address(this).balance;    
     }
     
-    function withdraw() public _onlyParticipant returns(bool) {
-        uint amount = participantStorage[msg.sender].pendingWithdrawals;
+    function withdraw() public _onlyIfRunning returns(bool) {
+        uint amount = accountStorage[msg.sender].pendingWithdrawals;
         
-        participantStorage[msg.sender].pendingWithdrawals = 0;
+        accountStorage[msg.sender].pendingWithdrawals = 0;
         msg.sender.transfer(amount);
         
         emit LogWithdrawed(msg.sender);
