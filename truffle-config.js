11d305349a9b549a5e3034d4b993ff716e91dd01
nitrax <jean.lejeune@protonmail.com>
2019-07-21 12:03:51 +0200

    Revamping skeleton

---
+++ truffle-config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  compilers: {
+      solc: {
+          version: "0.5.10",
+      }
+  }
+  // networks: {
+  //     development: {
+  //         host: "127.0.0.1",
+  //         port: 8545,
+  //         network_id: "*",
+  //     }
+  // }
+module.exports = {
+  compilers: {
+    solc: {
+      version: "0.5.10",
+    }
+  }
+  // networks: {
+  //     development: {
+  //         host: "127.0.0.1",
+  //         port: 8545,
+  //         network_id: "*",
+  //     }
+  // }
+};
