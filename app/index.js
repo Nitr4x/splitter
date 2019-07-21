54ad6e2918527e797812d743516e213e5567c90f
nitrax <jean.lejeune@protonmail.com>
2019-07-21 16:28:39 +0200

    First MVP

---
+++ app/index.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import Splitter from './vues/Splitter'
+
+new Vue({
+    el: '#app',
+    render: h => h(Splitter)
+});
--- truffle-config.js
+++ truffle-config.js
@@ -3,12 +3,12 @@ module.exports = {
     solc: {
       version: "0.5.10",
     }
+  },
+  networks: {
+      development: {
+          host: "127.0.0.1",
+          port: 8545,
+          network_id: "*",
+      }
   }
-  // networks: {
-  //     development: {
-  //         host: "127.0.0.1",
-  //         port: 8545,
-  //         network_id: "*",
-  //     }
-  // }
 };
